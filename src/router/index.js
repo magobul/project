82de19c00f77c6c2256c44968c3c4dc201252400
magobul <magobul@gmail.com>
2026-06-20 21:54:04 +0700

    updates

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-const routes = [
-  {
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
     path: '/',
     name: 'Home',
     component: () => import('../views/Home.vue')
@@ -62,10 +63,6 @@ const routes = [
     component: () => import('../views/AdminPanel.vue')
   }
 ]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
 })
 
 export default router
