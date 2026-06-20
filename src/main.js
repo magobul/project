5a4e20de1dcd88c819c0a1586c75fd2c7d82cae8
magobul <magobul@gmail.com>
2026-06-20 21:39:51 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { startKeepAlive } from './utils/keepAlive'
 
+// Запускаем пинг сервера
+startKeepAlive()
 createApp(App).use(router).mount('#app')
